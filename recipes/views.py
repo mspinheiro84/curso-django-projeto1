8c762e7b0f46fbda1576ccc37fc839e1d71af581
Marcelo Pinheiro <pinheiro84@yahoo.com.br>
2025-05-06 18:20:59 -0300

    Criação dos templates contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,22 @@
+"""Importações."""
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
+
 def home(request):
-    return render(request, 'global/home.html')
+    """Rota para home."""
+    return render(request, 'recipes/home.html', context={
+        'name': 'Marcelo Pinheiro',
+    })
+
 
 def contato(request):
-    return HttpResponse('Contato - Recipes')
+    """Rota para contato."""
+    return render(request, 'recipes/contato.html')
+
 
 def sobre(request):
-    return HttpResponse('Sobre - Recipes')
+    """Rota para sobre."""
+    return render(request, 'recipes/sobre.html')
