59251c18cc0fde2526fe36ab4abfc7e3f0598e0f
Marcelo Pinheiro <pinheiro84@yahoo.com.br>
2025-05-14 17:47:19 -0300

    templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 """Importações."""
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -8,12 +9,13 @@ from django.shortcuts import render
 def home(request):
     """Rota para home."""
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Marcelo Pinheiro',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipes(request, id):
     """Rota para home."""
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Marcelo Pinheiro',
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
