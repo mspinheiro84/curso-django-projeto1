e19886f065303e4b4f5d7c4d7f9d0c58ea65fe06
Marcelo Pinheiro <pinheiro84@yahoo.com.br>
2025-05-14 18:29:16 -0300

    Configuração do admin

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self): 
+        return self.name
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -17,3 +25,5 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
